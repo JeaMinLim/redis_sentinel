996e911f7172faf19d657b9508cbfcbe8fdbf74d
JeaMinLim <gam1532@gmail.com>
2022-05-27 17:07:48 +0900

    Add code to parse sentinel node info from application.yaml.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Before run this code, execute redis in Sentinel mode first.
 To change Server IP address or port, please edit _**application.yaml**_, or you can write on _**Config.java**_.
 To change Key-value, please edit _**application.yaml**_, or you can write on _**Controller.java**_.
 
+For supporting application.yaml in Sentinel mode, I add string parsing codes for **lettuceConnectionFactory()** in _**Config.java**_  
 
 I also upload test project for Redis Sentinel and Cluster mode.
 
--- src/main/java/test/jmlim/redis/sentinel/SentinelConfig.java
+++ src/main/java/test/jmlim/redis/sentinel/SentinelConfig.java
@@ -14,27 +14,49 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class SentinelConfig {
-    //@Value("${spring.redis.host}")
-    //private String HOSTNAME;
-    //@Value("${spring.redis.port}")
-    //private int PORT;
 
+    @Value("${spring.redis.sentinel.master}")
+    private String NAMEOFMASTER;
+    @Value("${spring.redis.sentinel.nodes}")
+    private String NODES;
+    private String[] SENTINELHOST;
+    private int[] SENTINELPORT;
 
+    /* manually add Sentinel`s info
+    private String NAMEOFMASTER = "RedisSentinel";
     private String SENTINELHOST1 = "127.0.0.1";
     private String SENTINELHOST2 = "127.0.0.1";
     private String SENTINELHOST3 = "127.0.0.1";
     private int SENTINELPORT1 = 26379;
     private int SENTINELPORT2 = 26380;
     private int SENTINELPORT3 = 26381;
+     */
 
     @Bean
     public RedisConnectionFactory lettuceConnectionFactory() {
+        // this is only need when Sentinel hostname and port is in application.yaml.
+        //System.out.println(NODES);
+        String[] _NODES = NODES.split(", ");
+        int _length = _NODES.length;
+        //System.out.println(_length);
+        SENTINELHOST = new String[_length];
+        SENTINELPORT = new int[_length];
+
+        for(int i=0; i<_length; i++) {
+            //System.out.println(_NODES[i]);
+            String[] __NODES = _NODES[i].split(":");
+            SENTINELHOST[i] = __NODES[0];
+            SENTINELPORT[i] = Integer.parseInt(__NODES[1]);
+            //System.out.println(SENTINELHOST[i] + " : "+ SENTINELPORT[i]);
+        }
+        // Until here!
+
         RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-                .master("RedisSentinel")
-                .sentinel(SENTINELHOST1, SENTINELPORT1)
-                .sentinel(SENTINELHOST2, SENTINELPORT2)
-                .sentinel(SENTINELHOST3, SENTINELPORT3)
-                ;
+                .master(NAMEOFMASTER)
+                .sentinel(SENTINELHOST[0], SENTINELPORT[0])
+                .sentinel(SENTINELHOST[1], SENTINELPORT[1])
+                .sentinel(SENTINELHOST[2], SENTINELPORT[2])
+                ; // Add more .sentinel() if there is more then 3 sentinels.
         return new LettuceConnectionFactory(sentinelConfig);
     }
 
